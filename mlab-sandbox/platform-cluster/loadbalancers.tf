715e65e38cfc2fa8274bb364dc86841eb3762fa0
Nathan Kinkade <kinkade@measurementlab.net>
2023-04-14 17:04:28 -0600

    Adds load balancer for ePoxy extension server
    The new epoxy extension server replaces the old token-server and
    bmc-store-password server services with a single service running on a
    single port. This commit renames the old token-server resources to use
    epoxy-extension-server and deletes the bmc-store-password load balancer
    resources.

--- mlab-sandbox/platform-cluster/loadbalancers.tf
+++ mlab-sandbox/platform-cluster/loadbalancers.tf
@@ -92,92 +92,47 @@ resource "google_compute_forwarding_rule" "platform_cluster" {
 
 
 #
-# Internal load balancer for ePoxy extension "allocate_k8s_token". The
-# token_server service runs on each of the control plane nodes.
+# Internal load balancer for the ePoxy extension server. The
+# extension server service runs on each of the control plane nodes.
 #
-resource "google_compute_address" "token_server_lb" {
+resource "google_compute_address" "epoxy_extension_server" {
   address_type = "INTERNAL"
-  name         = "token-server-lb"
+  name         = "epoxy-extension-server"
   region       = var.api_instances.machine_attributes.region
   subnetwork   = google_compute_subnetwork.platform_cluster["${var.api_instances.machine_attributes.region}"].id
 }
 
-resource "google_compute_health_check" "token_server" {
+resource "google_compute_health_check" "epoxy_extension_server" {
   tcp_health_check {
     port               = "8800"
     port_specification = "USE_FIXED_PORT"
   }
 
-  name = "token-server"
+  name = "epoxy-extension-server"
 }
 
-resource "google_compute_region_backend_service" "token_server" {
+resource "google_compute_region_backend_service" "epoxy_extension_server" {
   dynamic "backend" {
     for_each = google_compute_instance_group.platform_cluster
     content {
       group = backend.value.id
     }
   }
-  health_checks         = [google_compute_health_check.token_server.id]
+  health_checks         = [google_compute_health_check.epoxy_extension_server.id]
   load_balancing_scheme = "INTERNAL"
-  name                  = "token-server"
+  name                  = "epoxy-extension-server"
   protocol              = "TCP"
   region                = var.api_instances.machine_attributes.region
 }
 
-resource "google_compute_forwarding_rule" "token_server" {
+resource "google_compute_forwarding_rule" "epoxy_extension_server" {
   allow_global_access   = true
-  backend_service       = google_compute_region_backend_service.token_server.id
-  ip_address            = google_compute_address.token_server_lb.id
+  backend_service       = google_compute_region_backend_service.epoxy_extension_server.id
+  ip_address            = google_compute_address.epoxy_extension_server.id
   load_balancing_scheme = "INTERNAL"
-  name                  = "token-server"
+  name                  = "epoxy-extension-server"
   network               = google_compute_network.platform_cluster.id
   ports                 = ["8800"]
-  region                = var.api_instances.machine_attributes.region
-  subnetwork            = google_compute_subnetwork.platform_cluster["${var.api_instances.machine_attributes.region}"].id
-}
-
-#
-# Internal load balancer for ePoxy extension "bmc_store_password". The
-# bmc_store_password service runs on each of the control plane nodes.
-#
-resource "google_compute_address" "bmc_store_password_lb" {
-  address_type = "INTERNAL"
-  name         = "bmc-store-password-lb"
-  region       = var.api_instances.machine_attributes.region
-  subnetwork   = google_compute_subnetwork.platform_cluster["${var.api_instances.machine_attributes.region}"].id
-}
-
-resource "google_compute_health_check" "bmc_store_password" {
-  tcp_health_check {
-    port               = "8801"
-    port_specification = "USE_FIXED_PORT"
-  }
-
-  name = "bmc-store-password"
-}
-
-resource "google_compute_region_backend_service" "bmc_store_password" {
-  dynamic "backend" {
-    for_each = google_compute_instance_group.platform_cluster
-    content {
-      group = backend.value.id
-    }
-  }
-  health_checks         = [google_compute_health_check.bmc_store_password.id]
-  load_balancing_scheme = "INTERNAL"
-  name                  = "bmc-store-password"
-  protocol              = "TCP"
-  region                = var.api_instances.machine_attributes.region
-}
-
-resource "google_compute_forwarding_rule" "bmc_store_password" {
-  backend_service       = google_compute_region_backend_service.bmc_store_password.id
-  ip_address            = google_compute_address.bmc_store_password_lb.id
-  load_balancing_scheme = "INTERNAL"
-  name                  = "bmc-store-password"
-  network               = google_compute_network.platform_cluster.id
-  ports                 = ["8801"]
   region                = var.api_instances.machine_attributes.region
   subnetwork            = google_compute_subnetwork.platform_cluster["${var.api_instances.machine_attributes.region}"].id
 }
