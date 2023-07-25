73aa4a7055e25e1ff2a4f7b6bda946d39e21b8ad
Nathan Kinkade <kinkade@measurementlab.net>
2023-07-25 11:40:02 -0600

    Fixes IPv6 script description

--- scripts/assign_static_ipv6.sh
+++ scripts/assign_static_ipv6.sh
@@ -2,8 +2,8 @@
 #
 # A small shell script which will remove an ephemeral IPv6 address from an
 # instance and reassign a static IPv6 to the same instance. This is a temporary
-# helper script which will need to be run manually when instances are destroyed
-# and recreated. GCP supports static regional IPv6 addresses, but the Google
+# helper script which is run as a local-exec provisioner when instances are
+# [re]created by TF. GCP supports static regional IPv6 addresses, but the Google
 # Terraform provider does not yet support this.
 
 set -euxo pipefail
