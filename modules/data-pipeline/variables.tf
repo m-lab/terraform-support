8babf336aef972bea09f81228ff1b9bcd63cec61
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-08-23 16:57:00 -0400

    Remove data-pipeline region variable

--- modules/data-pipeline/variables.tf
+++ modules/data-pipeline/variables.tf
@@ -3,11 +3,6 @@ variable "project" {
   type        = string
 }
 
-variable "default_region" {
-  description = "Default GCP region"
-  type        = string
-}
-
 variable "default_location" {
   description = "Default GCP location"
   type        = string
