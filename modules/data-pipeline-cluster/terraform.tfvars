aade8e3db0bbf901b41df5c291b9d4bcfc05df70
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-08-23 14:42:48 -0400

    Add variables for project and region

---
+++ modules/data-pipeline-cluster/terraform.tfvars
@@ -0,0 +1,2 @@
+project        = "mlab-sandbox"
+default_region = "us-central1"
---
+++ modules/data-pipeline-cluster/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "GCP project"
+  type        = string
+}
+
+variable "default_region" {
+  description = "Default GCP region"
+  type        = string
+}
