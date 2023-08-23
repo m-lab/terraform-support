b0e032b11324f069aecffd5db30664859a1ee83f
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-08-23 15:20:04 -0400

    inline variables

--- modules/data-pipeline-cluster/main.tf
+++ modules/data-pipeline-cluster/main.tf
@@ -1,8 +1,13 @@
 provider "google" {
-  project = "mlab-sandbox"
+  project = var.project
+  region = var.default_region
 }
 
 
 module "data-pipeline-cluster-us-central1" {
   source = "./data-pipeline"
+
+  project = "mlab-sandbox"
+  default_region = "us-central1"
+  default_location = "us-central1"
 }
