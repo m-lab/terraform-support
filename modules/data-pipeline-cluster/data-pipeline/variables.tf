c9a60fe4d4a5e38621338fe98ae35ca0a53cb69e
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-08-23 15:19:50 -0400

    Add limited variable definitions

---
+++ modules/data-pipeline-cluster/data-pipeline/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "GCP project"
+  type        = string
+}
+
+variable "default_region" {
+  description = "Default GCP region"
+  type        = string
+}
+
+variable "default_location" {
+  description = "Default GCP location"
+  type        = string
+}
