e64171c5009c9f6478899e0349438c781c325a08
Nathan Kinkade <kinkade@measurementlab.net>
2023-12-13 16:11:05 -0700

    Adds new mlab-autojoin project directory
    This new project root module imports the data-pipeline module, and deploys two
    new node pools: "processor" and "prometheus".

---
+++ mlab-autojoin/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "gcs" {
+    # Terraform does not allow variable interpolation in backend blocks.
+    bucket = "terraform-support-mlab-autojoin"
+    prefix = "state"
+  }
+}
+
+provider "google" {
+  project = "mlab-autojoin"
+}
+
+provider "google" {
+  alias   = "data-pipeline"
+  project = "mlab-autojoin"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
