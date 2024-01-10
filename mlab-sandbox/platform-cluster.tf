cf84784e82f3ac663f00e845b17d54fef288c8c4
nkinkade <kinkade@measurementlab.net>
2024-01-10 12:57:05 -0700

    Updates disk images for sandbox and staging (#56)
    The changes are to fix a bug that was discovered:
    
    https://github.com/m-lab/ops-tracker/issues/1894

--- mlab-sandbox/platform-cluster.tf
+++ mlab-sandbox/platform-cluster.tf
@@ -8,7 +8,7 @@ module "platform-cluster" {
   instances = {
     attributes = {
       daemonset        = "ndt"
-      disk_image       = "platform-cluster-instance-2023-12-20t21-45-52"
+      disk_image       = "platform-cluster-instance-2024-01-09t21-00-54"
       disk_size_gb     = 100
       disk_type        = "pd-ssd"
       machine_type     = "n2-highcpu-4"
@@ -39,7 +39,7 @@ module "platform-cluster" {
 
   api_instances = {
     machine_attributes = {
-      disk_image        = "platform-cluster-api-instance-2023-12-20t21-45-52"
+      disk_image        = "platform-cluster-api-instance-2024-01-09t21-00-54"
       disk_size_gb_boot = 100
       disk_size_gb_data = 10
       # This will show up as /dev/disk/by-id/google-<name>
@@ -73,7 +73,7 @@ module "platform-cluster" {
   }
 
   prometheus_instance = {
-    disk_image        = "platform-cluster-internal-instance-2023-12-20t21-45-52"
+    disk_image        = "platform-cluster-internal-instance-2024-01-09t21-00-54"
     disk_size_gb_boot = 100
     disk_size_gb_data = 200
     disk_type         = "pd-ssd"
