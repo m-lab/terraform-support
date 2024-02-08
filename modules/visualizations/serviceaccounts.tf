547d8b31bd9a45ba1a6b19e76c5b454567f14b25
Nathan Kinkade <kinkade@measurementlab.net>
2024-02-08 16:25:42 -0700

    Adds new visualizations submodule
    The immediate use of this submodule will be to configure the proper service
    accounts for the grafana-public AppEngine instance.

---
+++ modules/visualizations/serviceaccounts.tf
@@ -0,0 +1,65 @@
+#
+# Service Accounts
+#
+resource "google_service_account" "grafana_public" {
+  account_id   = "grafana-public"
+  description  = "Account for public Grafana instance. Member of discuss@"
+  display_name = "grafana-public"
+}
+
+#
+# Role Bindings
+#
+resource "google_project_iam_binding" "logging_logwriter" {
+  project = data.google_project.current.id
+  role    = "roles/logging.logWriter"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "cloudtrace_agent" {
+  project = data.google_project.current.id
+  role    = "roles/cloudtrace.agent"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "monitoring_metricwriter" {
+  project = data.google_project.current.id
+  role    = "roles/monitoring.metricWriter"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "monitoring_metricsscopesviewer" {
+  project = data.google_project.current.id
+  role    = "roles/monitoring.metricsScopesViewer"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "storage_objectviewer" {
+  project = data.google_project.current.id
+  role    = "roles/storage.objectViewer"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
+
+resource "google_project_iam_binding" "artifactregistry_reader" {
+  project = data.google_project.current.id
+  role    = "roles/artifactregistry.reader"
+
+  members = [
+    "serviceAccount:grafana-public@mlab-sandbox.iam.gserviceaccount.com"
+  ]
+}
