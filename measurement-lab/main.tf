d47ed6e613a1f7d979c1f28e3568b103219f9a35
Nathan Kinkade <kinkade@measurementlab.net>
2024-03-05 15:25:28 -0700

    Adds new project root module for measurement-lab
    As a start, it adds the visualizations module, since the public Grafana
    instance will run in that project.

---
+++ measurement-lab/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  backend "gcs" {
+    # Terraform does not allow variable interpolation in backend blocks.
+    bucket = "terraform-support-measurement-lab"
+    prefix = "state"
+  }
+}
+
+provider "google" {
+  project = "measurement-lab"
+}
+
+provider "google" {
+  alias   = "platform-cluster"
+  project = "measurement-lab"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+provider "google" {
+  alias   = "data-pipeline"
+  project = "measurement-lab"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+provider "google" {
+  alias   = "visualizations"
+  project = "measurement-lab"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
