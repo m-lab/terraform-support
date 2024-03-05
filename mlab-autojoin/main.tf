c9d6f41b6ec9cd4b955565747113867168f5c481
Nathan Kinkade <kinkade@measurementlab.net>
2024-03-05 15:28:59 -0700

    Adds provider alias defs for platform-cluster and visualizations
    versions.tf is shared between all projects, and in that file any
    configuration_aliases that are listed, must be defined in the root module, even
    if those aliases are not used.

--- mlab-autojoin/main.tf
+++ mlab-autojoin/main.tf
@@ -16,3 +16,17 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
+
+provider "google" {
+  alias   = "platform-cluster"
+  project = "mlab-autojoin"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
+
+provider "google" {
+  alias   = "visualizations"
+  project = "mlab-autojoin"
+  region  = "us-central1"
+  zone    = "us-central1-a"
+}
