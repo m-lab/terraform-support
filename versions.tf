3cc23f840786c224d87507d388a96e6937a7400e
Roberto D'Auria <roberto@measurementlab.net>
2024-08-13 14:23:47 +0200

    Upgrade google provider to latest v5.40.0

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.12.0"
+      version = "5.40.0"
       configuration_aliases = [
         google.data-pipeline,
         google.foundations,
