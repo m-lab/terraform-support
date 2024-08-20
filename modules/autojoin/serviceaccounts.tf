1ec52d7b7ee0de369eb8e11b367ddbef2682c800
Roberto D'Auria <roberto@measurementlab.net>
2024-08-20 19:09:57 +0200

    Allow the autonode user to access the downloader-$PROJECT GCS bucket

--- modules/autojoin/serviceaccounts.tf
+++ modules/autojoin/serviceaccounts.tf
@@ -8,3 +8,9 @@ resource "google_storage_bucket_iam_member" "autonode_access" {
   role = "roles/storage.objectAdmin"
   member = "serviceAccount:${google_service_account.autonode.email}"
 }
+
+resource "google_storage_bucket_iam_member" "autonode_access_downloader" {
+  bucket = "downloader-${data.google_client_config.current.project}"
+  role = "roles/storage.objectViewer"
+  member = "serviceAccount:${google_service_account.autonode.email}"
+}
