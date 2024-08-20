98c9d50174d6eb313a373c70fc767dc30c6b0f5f
Roberto D'Auria <roberto@measurementlab.net>
2024-08-20 23:21:32 +0200

    Create Redis instance + move everything in the autojoin VPC network (#93)
    * Allow the autonode user to access the downloader-$PROJECT GCS bucket
    
    * Create Redis instance + move everything in the autojoin VPC network
    
    * Rename autojoin_vpc_network to just autojoin

--- modules/autojoin/firewall.tf
+++ modules/autojoin/firewall.tf
@@ -1,3 +1,16 @@
+# Allow IAP to SSH into every VM in the autojoin network.
+resource "google_compute_firewall" "iap_access" {
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  
+  description   = "Allow IAP to SSH into every VM in the autojoin network"
+  name          = "iap-access"
+  network       = google_compute_network.autojoin.name
+  source_ranges = ["35.235.240.0/20"]
+}
+
 # Allow open access to prometheus metrics on select servers
 resource "google_compute_firewall" "public_prometheus_monitoring" {
   allow {
@@ -7,7 +20,7 @@ resource "google_compute_firewall" "public_prometheus_monitoring" {
   
   description   = "Allow open access to prometheus metrics on select servers"
   name          = "public-prometheus-monitoring"
-  network       = "default"
+  network       = google_compute_network.autojoin.name
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["public-prometheus-monitoring"]
 }
