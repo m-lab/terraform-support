8d0608f666d101e80aaa0082c6b5ba5f9598f611
Roberto D'Auria <roberto@measurementlab.net>
2024-08-21 19:16:28 +0200

    Use default value for node_pools

--- mlab-sandbox/autojoin.tf
+++ mlab-sandbox/autojoin.tf
@@ -5,21 +5,6 @@ module "autojoin" {
     google = google.autojoin
   }
 
-  node_pools = {
-    "pipeline" = {
-      initial_node_count = 1
-      machine_type       = "n2-standard-4"
-      max_node_count     = 3
-      max_surge          = 1
-    },
-    "monitoring" = {
-      initial_node_count = 1
-      machine_type       = "n2-standard-4"
-      max_node_count     = 3
-      max_surge          = 1
-    }
-  }
-
   # On sandbox, GAE was initially set up to be on us-east1, and this cannot
   # be easily changed, se we override the appengine_region here.
   appengine_region = "us-east1"
--- modules/autojoin/variables.tf
+++ modules/autojoin/variables.tf
@@ -1,5 +1,19 @@
 variable "node_pools" {
   description = "Cluster node pools"
+  default = {
+    "pipeline" = {
+      initial_node_count = 1
+      machine_type       = "n2-standard-4"
+      max_node_count     = 3
+      max_surge          = 1
+    },
+    "monitoring" = {
+      initial_node_count = 1
+      machine_type       = "n2-standard-4"
+      max_node_count     = 3
+      max_surge          = 1
+    }
+  }
   type = map(
     object({
       initial_node_count = number
