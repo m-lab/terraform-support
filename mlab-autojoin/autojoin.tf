39a5ab0cebb97670316132eee630f1a21394623f
Roberto D'Auria <roberto@measurementlab.net>
2024-08-21 02:33:23 +0200

    Add required variables to the mlab-autojoin project, too.

--- mlab-autojoin/autojoin.tf
+++ mlab-autojoin/autojoin.tf
@@ -4,4 +4,19 @@ module "autojoin" {
   providers = {
     google = google.autojoin
   }
+
+  node_pools = {
+    "pipeline" = {
+      initial_node_count = 1
+      machine_type       = "n2-standard-4"
+      max_node_count     = 3
+      max_surge          = 1
+    },
+    "monitoring" = {
+      initial_node_count = 1
+      machine_type       = "n2-standard-4"
+      max_node_count     = 3
+      max_surge          = 1
+    }
+  }
 }
