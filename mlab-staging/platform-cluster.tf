dbbfe7fff3456615f0232a4f7cd033267ab993c1
Nathan Kinkade <kinkade@measurementlab.net>
2024-09-18 17:40:33 -0600

    Updates staging disk images for platform cluster
    Upgrades to k8s 1.28.14

--- mlab-staging/platform-cluster.tf
+++ mlab-staging/platform-cluster.tf
@@ -8,7 +8,7 @@ module "platform-cluster" {
   instances = {
     attributes = {
       daemonset        = "ndt"
-      disk_image       = "platform-cluster-instance-2024-09-11t19-56-13"
+      disk_image       = "platform-cluster-instance-2024-09-18t21-40-07"
       disk_size_gb     = 100
       disk_type        = "pd-ssd"
       machine_type     = "n2-highcpu-4"
@@ -38,7 +38,7 @@ module "platform-cluster" {
 
   api_instances = {
     machine_attributes = {
-      disk_image        = "platform-cluster-api-instance-2024-09-11t19-56-13"
+      disk_image        = "platform-cluster-api-instance-2024-09-18t21-40-07"
       disk_size_gb_boot = 100
       disk_size_gb_data = 10
       # This will show up as /dev/disk/by-id/google-<name>
@@ -72,7 +72,7 @@ module "platform-cluster" {
   }
 
   prometheus_instance = {
-    disk_image        = "platform-cluster-internal-instance-2024-09-11t19-56-13"
+    disk_image        = "platform-cluster-internal-instance-2024-09-18t21-40-07"
     disk_size_gb_boot = 100
     disk_size_gb_data = 1500
     disk_type         = "pd-ssd"
