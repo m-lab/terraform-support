64176e12d479b0a3f05e94579354ee43879d3e26
Stephen Soltesz <stephen.soltesz@gmail.com>
2024-09-19 13:38:27 -0400

    Add GCS and BigQuery roles for autoloader to autojoin service accounts (#101)
    * Add gcs and bq roles for autoloader
    * Use custom role for storage.buckets.get
    * Add bigquery.dataEditor and .user for autoloader
    * Update nodepool name to match data-pipeline

--- modules/autojoin/roles.tf
+++ modules/autojoin/roles.tf
@@ -1,3 +1,19 @@
+resource "google_project_iam_custom_role" "autoloader_gcs_reader" {
+  description = "autolaoder-gcs-reader"
+  # NOTE: permissions mirror the set used by legacy reader, a role that cannot
+  # be assigned to service accounts.
+  permissions = [
+    "storage.buckets.get",
+    "storage.managedFolders.get",
+    "storage.managedFolders.list",
+    "storage.multipartUploads.list",
+    "storage.objects.list",
+  ]
+  role_id     = "autoloader_gcs_access"
+  stage       = "GA"
+  title       = "autoloader-gcs-reader"
+}
+
 resource "google_storage_bucket_iam_member" "autonode_access" {
   bucket = "archive-${data.google_client_config.current.project}"
   role = "roles/storage.objectAdmin"
@@ -24,6 +40,24 @@ resource "google_project_iam_member" "autonode_gke_container_registry_access" {
 
 resource "google_project_iam_member" "autonode_gke_default_node_permissions" {
   role = "roles/container.defaultNodeServiceAccount"
+  member = "serviceAccount:${google_service_account.gke.email}"
+  project = data.google_client_config.current.project
+}
+
+resource "google_project_iam_member" "autonode_gke_gcs_reader" {
+  role = "${google_project_iam_custom_role.autoloader_gcs_reader.id}"
+  member = "serviceAccount:${google_service_account.gke.email}"
+  project = data.google_client_config.current.project
+}
+
+resource "google_project_iam_member" "autonode_gke_bigquery_updater" {
+  role = "roles/bigquery.dataEditor"
+  member = "serviceAccount:${google_service_account.gke.email}"
+  project = data.google_client_config.current.project
+}
+
+resource "google_project_iam_member" "autonode_gke_bigquery_user" {
+  role = "roles/bigquery.user"
   member = "serviceAccount:${google_service_account.gke.email}"
   project = data.google_client_config.current.project
 }
