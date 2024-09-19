0fce029fc24391c38630323b141f9fa078f35c35
Roberto D'Auria <roberto@measurementlab.net>
2024-09-19 16:46:36 +0200

    Move to roles.tf, add container.defaultNodeServiceAccount to autojoin GKE sa

--- modules/autojoin/serviceaccounts.tf
+++ modules/autojoin/serviceaccounts.tf
@@ -9,27 +9,3 @@ resource "google_service_account" "gke" {
   description = "Default SA for the autojoin GKE cluster node pools (managed by Terraform)"
   display_name = "Autojoin GKE SA"
 }
-
-resource "google_project_iam_member" "autonode_gke_artifact_registry_access" {
-  role = "roles/artifactregistry.reader"
-  member = "serviceAccount:${google_service_account.gke.email}"
-  project = data.google_client_config.current.project
-}
-
-resource "google_project_iam_member" "autonode_gke_container_registry_access" {
-  role = "roles/containerregistry.ServiceAgent"
-  member = "serviceAccount:${google_service_account.gke.email}"
-  project = data.google_client_config.current.project
-}
-
-resource "google_storage_bucket_iam_member" "autonode_access" {
-  bucket = "archive-${data.google_client_config.current.project}"
-  role = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.autonode.email}"
-}
-
-resource "google_storage_bucket_iam_member" "autonode_access_downloader" {
-  bucket = "downloader-${data.google_client_config.current.project}"
-  role = "roles/storage.objectViewer"
-  member = "serviceAccount:${google_service_account.autonode.email}"
-}
