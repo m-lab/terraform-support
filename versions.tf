b1b5dd7619cb0624a3de902cc66d345f87610c8e
Nathan Kinkade <kinkade@measurementlab.net>
2025-11-04 16:13:36 -0700

    Adds back erroneously deleted configuration_alias definitions

--- versions.tf
+++ versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/google"
       version = "7.3.0"
       configuration_aliases = [
+        google.data-pipeline,
+        google.foundations,
+        google.platform-cluster,
+        google.visualizations
         google.google-oim
       ]
     }
