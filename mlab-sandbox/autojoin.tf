759894f8c724c7411ba600d4677cf2e5c86d1950
Roberto D'Auria <roberto@measurementlab.net>
2026-09-02 14:31:15 +0200

    fix(autojoin): move autonode-deb to us-central1-b, us-central1-a is stocked out

--- mlab-sandbox/autojoin.tf
+++ mlab-sandbox/autojoin.tf
@@ -10,8 +10,8 @@ module "autojoin" {
   appengine_region = "us-east1"
 
   # Test VM for the mlab-node Debian package (byos-debian). Not in
-  # us-central1-c (the provider default) because that zone was out of
+  # us-central1-c (the provider default) or us-central1-a: both were out of
   # n2-standard-2 capacity at creation time.
   deploy_autonode_deb = true
-  autonode_deb_zone   = "us-central1-a"
+  autonode_deb_zone   = "us-central1-b"
 }
