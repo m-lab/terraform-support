68f2193db1d2121f1943636a612529d81de67136
Roberto D'Auria <roberto@measurementlab.net>
2026-09-14 23:40:33 +0200

    fix(autojoin): pin the mlab-staging autonode-deb VM to us-central1-a

--- mlab-staging/autojoin.tf
+++ mlab-staging/autojoin.tf
@@ -5,6 +5,9 @@ module "autojoin" {
     google = google.autojoin
   }
 
-  # Test VM for the mlab-node Debian package (byos-debian).
+  # Test VM for the mlab-node Debian package (byos-debian). Not in
+  # us-central1-c (the provider default) because that zone was out of
+  # n2-standard-2 capacity at creation time.
   deploy_autonode_deb = true
+  autonode_deb_zone   = "us-central1-a"
 }
